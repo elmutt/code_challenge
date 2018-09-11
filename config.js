be128c4d54ba149ab3b2d87e9b5a091bd6abd50b
seanluther <elmutt@gmail.com>
2018-09-10 18:51:18 -0600

    blah

--- config.js
+++ config.js
@@ -1,12 +1,18 @@
 
 module.exports = {
-  exchangeApiRequestTimeout: 10000, // exchanges must respond within 10 seconds
-  rateLimits: {
-    windowMs: 10 * 1000, // 5 seconds
+  // exchanges must respond within 10 seconds
+  exchangeApiRequestTimeout: 10000,
+  rateLimits: {    
+    // 10 requests per 10 seconds
+    windowMs: 10 * 1000,
     max: 10
   },
+  // Exchanges usually have 8 decimal places by default but this rarely results in orders being combined because the prices dont match.  
+  // Reduce precision to combine more orders
   pricePrecision: 4,
   port: 3001,
+  
+  // default base and quote symbols of order books to combine
   defaultBase: 'BTC',
   defaultQuote: 'ETH',
   
